8d9c70e9fa5416cecb43d07575bc1c3200d03a1b
Autistic <92280658+Astrues@users.noreply.github.com>
2022-04-14 23:26:12 +0800

    Update lv3.js

--- task4/lv3.js
+++ task4/lv3.js
@@ -1,20 +1,26 @@
+//网上搜到的模块，这两天npm装的很慢，就直接照着文档用了，不知道行不行
 class addReadme {
     constructor() {
-        const fs = require("fs");
-        fs.writeFile("README", "md", function (err) {
-            if (err) {
-                return console.log(err);
-            }
-        });
-        fs.renameSync("README", "README.md", function (err) {
-            if (err) {
-                return console.log(err);
-            }
-        });
-    }
-    apply(compiler) {
+        const Git = require("nodegit");
+        const getMostRecentCommit = function(repository) {
+          return repository.getBranchCommit("master");
+        };
+        const getCommitMessage = function(commit) {
+          return commit.message();
+        };
 
+        const message = Git.Repository.open("nodegit")
+              .then(getMostRecentCommit)
+              .then(getCommitMessage)
+              .then(function(message) {
+                return message
+              });
+        const fs = require("fs");
+        fs.writeFile("README.md",message, function (err) {
+            if (err) {
+                return console.log(err);
+            }
+        });
     }
 }
-const add = new addReadme();
 module.exports = addReadme;
